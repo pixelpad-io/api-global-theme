0a80adae86cc04120bfa0a60878b581dedd3f21a
Ubuntu <ubuntu@ip-172-31-2-45.us-west-1.compute.internal>
2021-11-26 23:10:05 +0000

    moved git to the api parent theme directory, not including the child theme

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
 <?php
-// Silence is golden.
+
+/* This file is the default template. It is rendered when the query is
+ * successful (i.e., not a 404), but there is no other more specific
+ * template to render the results. */
+
+?>
+
+<?php get_header(); ?>
+
+<?php /* PAGE CONTENT HERE */ ?>
+
+<?php get_footer(); ?>
