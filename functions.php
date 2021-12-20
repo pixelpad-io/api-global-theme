9f0c8b81d093d7bcf439d01fa3ba518e2a573b0f
Ubuntu <ubuntu@ip-172-31-2-45.us-west-1.compute.internal>
2021-12-20 10:50:41 +0000

    updated version. changed constant name for theme directory

--- functions.php
+++ functions.php
@@ -1,20 +1,18 @@
 <?php
 
-define("CHILD_THEME_URL", get_stylesheet_directory_uri());
-define("CHILD_THEME_DIR", get_stylesheet_directory());
-define("PARENT_THEME_DIR", dirname(__FILE__));
+define("GLOBAL_THEME_DIR", dirname(__FILE__));
 
 /**
  * load necessary front-end files
  */
 add_action("init", function () {
-    require_once(CHILD_THEME_DIR . "/class-front-page.php");
+    require_once(GLOBAL_THEME_DIR . "/class-front-page.php");
 });
 
 /**
  * load necessary admin files
  */
 add_action("admin_init", function () {
-    require_once(PARENT_THEME_DIR . "/class-updater.php");
+    require_once(GLOBAL_THEME_DIR . "/class-updater.php");
     $updater = new ThemeUpdater();
 });
