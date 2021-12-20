1fd973f678fc74ee9c8cde418c6eb5321a9e36ef
Ubuntu <ubuntu@ip-172-31-2-45.us-west-1.compute.internal>
2021-12-20 01:00:41 +0000

    slight optimization; checking updates only when in admin area

--- functions.php
+++ functions.php
@@ -2,17 +2,19 @@
 
 define("CHILD_THEME_URL", get_stylesheet_directory_uri());
 define("CHILD_THEME_DIR", get_stylesheet_directory());
-define("PARENT_THEME_DIR", dirname(__FILE__) );
+define("PARENT_THEME_DIR", dirname(__FILE__));
 
-
+/**
+ * load necessary front-end files
+ */
 add_action("init", function () {
-    /**
-     * load necessary files
-     */
     require_once(CHILD_THEME_DIR . "/class-front-page.php");
+});
+
+/**
+ * load necessary admin files
+ */
+add_action("admin_init", function () {
     require_once(PARENT_THEME_DIR . "/class-updater.php");
-
     $updater = new ThemeUpdater();
-
-}, 1);
-
+});
